97208b6ca41a8d17d4726a047fa6c024852569cc
9and3 <andreasettimi39@gmail.com>
2022-02-03 18:41:27 +0100

    last working verssion for pcd viewer

--- src/zed_test.py
+++ src/zed_test.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def main():
 
-    print("Running Augmented Stacking depth capture GL viewer... Press 'Esc' to quit")
+    
 
     # Set ZED params
     init = sl.InitParameters(camera_resolution=sl.RESOLUTION.HD720, # HD720 | 1280*720
@@ -16,7 +16,10 @@ def main():
                              depth_mode=sl.DEPTH_MODE.QUALITY, # posible mods: sl.DEPTH_MODE.PERFORMANCE/.QUALITY/.ULTRA
                              coordinate_units=sl.UNIT.METER,
                              coordinate_system=sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP, # sl.COORDINATE_SYSTEM.LEFT_HANDED_Y_UP
-                             sdk_verbose = True) # Enable verbose logging
+                             sdk_verbose = True, # Enable verbose logging
+                             depth_minimum_distance=0.3, # Enable capture from 30 cm
+                             depth_maximum_distance=4.0 # Enable capture up to 3m
+                             ) 
     
     # Open ZED
     zed = sl.Camera()
@@ -24,18 +27,19 @@ def main():
     if status != sl.ERROR_CODE.SUCCESS:
         print(repr(status))
         exit()
+    camera_info = zed.get_camera_information()
+    print("POP: ZED camera opened, serial number: {0}".format(camera_info.serial_number))
 
-    
     # Create OpenGL viewer, launch and set Viewer Camera params
     viewer = gl.GLViewer()
+    print("POP: Running Augmented Stacking depth capture GL viewer... Press 'Esc' to quit")
 
     camera_model = zed.get_camera_information().camera_model
     viewer.init(len(sys.argv), sys.argv, camera_model, zed.get_camera_information().camera_resolution)
     
-    viewer.bckgrnd_clr = (0, 0, 0)
-    viewer.camera.position_.init_vector(5, 0, -3) # // TEST
-    viewer.camera.setRotation(sl.Rotation().set_rotation_vector(0, 0, 180)) # 0,90,180 // TEST
-
+    viewer.bckgrnd_clr = np.array([170/255., 100/150., 180/255.])
+    viewer.camera.position_.init_vector(5., 0., -2.) # // TEST
+    viewer.camera.setRotation(sl.Rotation().set_rotation_vector(0., 0., 90.)) # 0,90,180 // TEST
 
 
     # Set point cloud object params from ZED frame
@@ -44,7 +48,7 @@ def main():
                          sl.MAT_TYPE.F32_C4,
                          sl.MEM.CPU)
 
-    # Feed point cloud to OpenGL viewer and refresh
+    # Main loop
     while viewer.is_available():
         if zed.grab() == sl.ERROR_CODE.SUCCESS:
             zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA,sl.MEM.CPU, zed.get_camera_information().camera_resolution) # for color: sl.MEASURE.XYZRGBA
