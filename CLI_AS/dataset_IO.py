d182a8e5d93074c15df87eae5c501ba0414e2f51
9and3 <andreasettimi39@gmail.com>
2022-03-01 15:16:55 +0100

    UPDATE: new folder with type ply mesh

--- CLI_AS/dataset_IO.py
+++ CLI_AS/dataset_IO.py
@@ -1,6 +1,52 @@
 
+'''
+The module implements the download and read of stone mesh/cloud data sets present in
+the cloud (posibly github) OR locally if necessary
+'''
+
+# Download a mesh object from git hub repository
+def download_mesh(url, filename):
+    import urllib.request
+    import tarfile
+    import os
+    import shutil
+    import sys
+    import platform
+
+    # Check if the file is already downloaded
+    if os.path.isfile(filename):
+        print("File already downloaded")
+        return
+
+    # Download the file
+    try:
+        print("Downloading file from: " + url)
+        mesh = urllib.request.urlretrieve(url, filename)
+    except:
+        print("Error downloading file")
+        sys.exit()
+    
+    print(dtype(mesh))
+    return mesh
 
 
+    # # Extract the file
+    # print("Extracting file")
+    # tar = tarfile.open(filename)
+    # tar.extractall()
+    # tar.close()
+
+    # # Remove the tar file
+    # os.remove(filename)
+
+    # # Rename the extracted folder
+    # if platform.system() == "Windows":
+    #     os.rename("stone_mesh", "stone_mesh_win")
+    # else:
+    #     os.rename("stone_mesh", "stone_mesh_linux")
+
+    # # Remove the extracted folder
+    # shutil.rmtree("stone_mesh_linux")
 
 def test():
     print("test")
