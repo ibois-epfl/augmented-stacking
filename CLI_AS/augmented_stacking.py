11a7561a9e8c0d461bab0eac28c503c45dbed0dd
9and3 <andreasettimi39@gmail.com>
2022-03-02 14:18:24 +0100

    add folder to testing c++ code stacking algorithm

--- CLI_AS/augmented_stacking.py
+++ CLI_AS/augmented_stacking.py
@@ -9,6 +9,15 @@ _name_mesh = 'bun_zipper.ply'
 
 
 def main():
+    
+    # V - [1] Download the low-res mesh
+    # >>> [2] Compute the mesh 6dof pose
+    # [3] Store/save the 6dof pose locally
+    # [4] Load the high-res mesh
+    # [5] Calculate deviation
+    # [6] Colored captured point cloud with rgb deviation values
+    # [8] Pass it to the 3D-2D pipeline
+
 
     # Download the mesh file
     dataset_IO.download_github_raw_file(_url_low_res_dir, _name_mesh)
@@ -17,7 +26,7 @@ def main():
     mesh = o3d.io.read_triangle_mesh(_name_mesh)
     o3d.visualization.draw_geometries([mesh])
 
-    dataset_IO.delete_file(_name_mesh)
+    # dataset_IO.delete_file(_name_mesh)
 
 
 if __name__ == "__main__":
