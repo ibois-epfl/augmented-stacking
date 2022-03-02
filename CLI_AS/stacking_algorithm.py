5456115f4123130dc21821e3aca00e72e4d5584a
9and3 <andreasettimi39@gmail.com>
2022-03-02 14:58:47 +0100

    add temp file

--- CLI_AS/stacking_algorithm.py
+++ CLI_AS/stacking_algorithm.py
@@ -1,20 +1,25 @@
 
+'''
+Moule to call EESD stacking algorithm to compute the 6dof pose of the rock and update the landscape
+For now the outputs are written to disk in ./temp folder because we don´t have pybinding
+'''
 
-def call_stacking_algorithm(path_to_mesh, landscape, config_file, output_dir):
-    """
-    Call the stacking algorithm
-    """
+import sys
 
-# Definition to run c++ file from terminal
-def stackingalogrithmc(path_exec, path_mesh, path_landscape, config_file, output_dir):
+def compute(path_exec, path_mesh, path_landscape, config_file, output_dir):
     """
     Call the stacking algorithm c++ file
 
+    :param path_exec: path to the c++ file
     :param path_mesh: path to the downloadedd low-res mesh
     :param path_landscape: path to the captured landscape mesh
     :param config_file: path to the config .txt file for parameters
     :param output_dir: path to the output directory
+
+    :return:
     """
-    # Call the c++ file
-    os.system(f"{path_exec} {path_toMesh} {landscape} {config_file} {output_dir}")
+    # Check for linux system
+    if sys.platform == 'linux':
+        # Call the c++ file via the command line
+        os.system(f"{path_exec} {path_mesh} {path_landscape} {config_file} {output_dir}")
     return
