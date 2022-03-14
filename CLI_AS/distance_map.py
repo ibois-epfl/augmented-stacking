da413bbc22bbc74c47271ed60083d3bc5bb69f35
qianqing.wanggg <qianqing.wanggg@gmail.com>
2022-03-14 15:32:02 +0100

    notimportant

--- CLI_AS/distance_map.py
+++ CLI_AS/distance_map.py
@@ -1,12 +1,11 @@
 
-import sys 
+import sys
 import open3d as o3d
 import numpy as np
 import color_map
 
 
 def compute(mesh, pc):
-
 
     # Load mesh and convert to open3d.t.geometry.TriangleMesh
     # mesh = o3d.io.read_triangle_mesh("./foundation.ply")
@@ -15,19 +14,20 @@ def compute(mesh, pc):
 
     # Create a scene and add the triangle mesh
     scene = o3d.t.geometry.RaycastingScene()
-    _ = scene.add_triangles(mesh) 
+    _ = scene.add_triangles(mesh)
 
     # Load point clound to be adjusted
     # pc = o3d.io.read_point_cloud("./A46_point_cloud.ply")
     #R = pc.get_rotation_matrix_from_xyz((np.pi / 6, 0, np.pi / 6))
-    #pc.rotate(R)
+    # pc.rotate(R)
 
     # compute distance
-    unsigned_distance = scene.compute_distance(np.asarray(pc.points, dtype = np.float32))
+    unsigned_distance = scene.compute_distance(
+        np.asarray(pc.points, dtype=np.float32))
 
     # normalize distance to (0,1)
-    if(unsigned_distance.numpy().max()<=0):
-        print("maximal distance should be non-zero!")
+    if(unsigned_distance.numpy().max() <= 0):
+        raise("maximal distance should be non-zero!")
         sys.exit(1)
 
     normalized_distance = unsigned_distance.numpy()/unsigned_distance.numpy().max()
@@ -37,7 +37,7 @@ def compute(mesh, pc):
     end_hue = 0
     colors = []
     for p in normalized_distance:
-        colors.append(color_map.transitionOfHueRange(p,start_hue,end_hue))
+        colors.append(color_map.transitionOfHueRange(p, start_hue, end_hue))
     c = o3d.utility.Vector3dVector(colors)
     pc.colors = c
 
