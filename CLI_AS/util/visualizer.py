ab7f32dc869e47279585840b6deb7f423d2aaeb0
9and3 <andreasettimi39@gmail.ch>
2022-04-05 16:45:08 +0200

    DEBUG: Added class concept of visualisation of the waal, for better control

--- CLI_AS/util/visualizer.py
+++ CLI_AS/util/visualizer.py
@@ -29,6 +29,7 @@ def viualize_mesh_normal(mesh, title):
     vis.run()
     vis.destroy_window()
 
+
 def viualize_wall(geometries, title):
     vis = o3d.visualization.Visualizer()
     vis.create_window(window_name=title,
@@ -43,3 +44,23 @@ def viualize_wall(geometries, title):
 
     vis.run()
     vis.destroy_window()
+
+## DEBUG changed function into class for destroying pruposes
+class viualize_wall_class(object):
+    def __init__(self,geometries, title):
+        self.vis = o3d.visualization.Visualizer()
+        self.vis.create_window(window_name=title,
+                        width=WALL_VIS_W,
+                        height=WALL_VIS_H)
+        for g in geometries:
+            self.vis.add_geometry(g)
+
+        opt = self.vis.get_render_option()
+        opt.background_color = [0, 0, 0]
+        opt.mesh_color_option = o3d.visualization.MeshColorOption.Normal
+
+        self.vis.run()
+    
+    def destroy(self):
+        self.vis.close()
+        self.vis.destroy_window()
