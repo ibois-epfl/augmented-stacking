93c6e43f80a4790ec453c50da3c421cd35aa2c42
9and3 <andreasettimi39@gmail.ch>
2022-04-08 13:49:27 +0200

    Upload of current files, after trying different projection solution, work is still in progress

--- CLI_AS/distance_map.py
+++ CLI_AS/distance_map.py
@@ -3,45 +3,68 @@ import sys
 import open3d as o3d
 import numpy as np
 import color_map
-
+from util import terminal
 
 def compute(mesh, pc):
 
     # Load mesh and convert to open3d.t.geometry.TriangleMesh
-    # mesh = o3d.io.read_triangle_mesh("./foundation.ply")
-
     mesh = o3d.t.geometry.TriangleMesh.from_legacy(mesh)
 
     # Create a scene and add the triangle mesh
     scene = o3d.t.geometry.RaycastingScene()
     _ = scene.add_triangles(mesh)
+    
+    if len(pc.points)!=0:
+        # compute distance
+        # unsigned_distance = scene.compute_distance(
+        #     np.asarray(pc.points, dtype=np.float32))
+        ## center of point cloud
+        ## center of mesh
 
-    # Load point clound to be adjusted
-    # pc = o3d.io.read_point_cloud("./A46_point_cloud.ply")
-    #R = pc.get_rotation_matrix_from_xyz((np.pi / 6, 0, np.pi / 6))
-    # pc.rotate(R)
+        # center = mesh.get_center()
+        # center = np.array(center.numpy())
+        # print(type(center))
+        # print(center)
 
-    # compute distance
-    unsigned_distance = scene.compute_distance(
-        np.asarray(pc.points, dtype=np.float32))
+        unsigned_distance = pc.compute_point_cloud_distance(pcd_center)
 
-    # normalize distance to (0,1)
-    if(unsigned_distance.numpy().max() <= 0):
-        raise("maximal distance should be non-zero!")
-        sys.exit(1)
+        # compute occupancy
+        # occupancy = scene.compute_occupancy(np.asarray(pc.points, dtype=np.float32))
 
-    normalized_distance = unsigned_distance.numpy()/unsigned_distance.numpy().max()
+        # normalize distance to (0,1)
+        if(unsigned_distance.numpy().max() <= 0):
+            raise("maximal distance should be non-zero!")
+            sys.exit(1)
 
-    # create color for each point according to the distance
-    start_hue = 120
-    end_hue = 0
-    colors = []
-    for p in normalized_distance:
-        colors.append(color_map.transitionOfHueRange(p, start_hue, end_hue))
-    c = o3d.utility.Vector3dVector(colors)
-    pc.colors = c
+        # normalized_distance = unsigned_distance.numpy()/unsigned_distance.numpy().max()
+        normalized_distance = unsigned_distance.numpy()
+        normalized_distance[normalized_distance>0.1] = 0.1
+        normalized_distance = normalized_distance/0.1
 
-    # write colored point cloud
-    # o3d.io.write_point_cloud("./colored_pc.ply", pc)
+        ### DEBUG Distance
+        print(f"Length of distances: {normalized_distance.shape}")
+        print(f"Max distance: {np.max(normalized_distance)}")
+        print(f"Min distance: {np.min(normalized_distance)}")
 
+        # create color for each point according to the distance
+        start_hue = 120
+        end_hue = 0
+        colors = []
+        for i,p in enumerate(normalized_distance):
+
+            colors.append(color_map.transitionOfHueRange(p, start_hue, end_hue))
+
+            # #Test with occupancy
+            # if occupancy[i]==1.0:
+            #     colors.append(color_map.transitionOfHueRange(p, start_hue, end_hue))
+            # else:
+            #     colors.append((1,0,1))
+        c = o3d.utility.Vector3dVector(colors)
+        pc.colors = c
+
+        # write colored point cloud
+        # o3d.io.write_spoint_cloud("./colored_pc.ply", pc)
+    else:
+        terminal.error_print("WARN, distance_map.py: captured croped point cloud is empty")
+        pc = None
     return pc
