eb27f15090e122dc28211c06ff99d81fe107d56e
2dav8 <david.rebiffe@outlook.fr>
2022-05-06 18:38:35 +0200

    file before modification

--- CLI_AS/augmented_stacking.py
+++ CLI_AS/augmented_stacking.py
@@ -41,8 +41,7 @@ _faces_target_stone_mesh = 500
 _vertices_target_low_res_scene = 2000
 
 # Set dir to store validated stone and landscape of each captured
-DIR_AS_BUILT_ASSEMBLY = './as_built_status'
-
+DIR_AS_BUILT_ASSEMBLY = './as_built_status/'
 
 def main():
     # -----------------------------------------------------------------------
@@ -67,7 +66,9 @@ def main():
 
     # Create sub-dir for current session
     dir_as_built = dataset_IO.create_record_session_subdir(DIR_AS_BUILT_ASSEMBLY)
-
+    
+    # Set stone stacked counter for recording
+    STONE_COUNTER = 0
 
     while(True):
 
@@ -116,10 +117,10 @@ def main():
                 _vertices_target_low_res_scene)
             
             # # Save meshed scene
-            print("Writing out the captured mesh from 3d camera")
+            terminal.custom_print("Writing out the captured mesh from 3d camera")
             o3d.io.write_triangle_mesh(
                 _name_landscape, landscape_mesh, write_ascii=True)
-            print(f"Mesh out with name {_name_landscape}")
+            terminal.custom_print(f"Mesh out with name {_name_landscape}")
 
             # Compute stacking pose
             stacking_algorithm.compute(path_exec='./stacking_algorithm/build/main',
@@ -141,23 +142,21 @@ def main():
 
             # Merge transformed stone and landscape mesh
             merged_landscape = stone_mesh + landscape_mesh
-            visualizer.viualize_wall([merged_landscape], 'wall view')  # DEBUG
+            # visualizer.viualize_wall([merged_landscape], 'wall view')  # DEBUG
 
-            # Check if the the stone is out of scope of the scene
-            bbox_landscape = landscape_mesh.get_axis_aligned_bounding_box()
-            bbox_merged_landscape = merged_landscape.get_axis_aligned_bounding_box()
-            if bbox_merged_landscape.volume() > bbox_landscape.volume()*2:
-                terminal.error_print("The stacking algorithm failed (stone far from scene)"
-                                    "You will be redirected to choose another stone ...")
-                continue
-            else:
-                break
+            # # Check if the the stone is out of scope of the scene
+            # bbox_landscape = landscape_mesh.get_axis_aligned_bounding_box()
+            # bbox_merged_landscape = merged_landscape.get_axis_aligned_bounding_box()
+            # if bbox_merged_landscape.volume() > bbox_landscape.volume()*2:
+            #     terminal.error_print("The stacking algorithm failed (stone far from scene)"
+            #                         "You will be redirected to choose another stone ...")
+            #     continue
+            # else:
+            break
 
         # -----------------------------------------------------------------------
         # [4] Augmented feedback loop
         # -----------------------------------------------------------------------
-
-        # visualizer.viualize_wall([merged_landscape], 'wall view')  # DEBUG
 
         # First open the camera and close at the end
         zed, point_cloud = camera_capture.set_up_zed()
@@ -165,7 +164,7 @@ def main():
         # Create the 3D space model
         Live_3D_space = camera_capture.Live_3D_space(rock_mesh=stone_mesh,zed=zed,point_cloud=point_cloud)
 
-        # create image object 
+        # Create image object 
         image_drawer = camera_capture.Image_drawer(Live_3D_space=Live_3D_space)
         
         # Start live 2D projection to guide user to stone placement
@@ -179,10 +178,11 @@ def main():
         # Ask to validate the stone and store current layer results
         i_is_pose_stone_valid = terminal.user_input('Do you validate the stone position? (y/n)\n>>> ')
         if i_is_pose_stone_valid in ['Y', 'y']:
-            dataset_IO.save_current_built_layer(landscape_mesh=landscape_mesh,
-                                                stone_mesh=stone_mesh,
-                                                stone_label=name_stone_mesh,
-                                                sub_dir=dir_as_built)
+            STONE_COUNTER = dataset_IO.save_current_built_layer(landscape_mesh=landscape_mesh,
+                                                                stone_mesh=stone_mesh,
+                                                                stone_label=name_stone_mesh,
+                                                                sub_dir=dir_as_built,
+                                                                stone_counter=STONE_COUNTER)
 
             # Ask to place another stone or terminate CLI
             i_continue_stacking = terminal.user_input('Do you want to stack another stone? y/n)\n>>> ')
